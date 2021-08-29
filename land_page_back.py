86e3bee1d6122154581f4ef9c584b1e07f306566
yehuda <ybsh10@gmail.com>
2021-08-29 14:25:13 +0300

    new mongodb and tests

--- land_page_back.py
+++ land_page_back.py
@@ -1,26 +1,55 @@
 import re
 import json
 from datetime import datetime
+from pymongo import MongoClient
+import consts
+
+
+def db_connection():
+
+    #TODO create secure environment variables or oder way to store secret info
+
+    conn_str = f"mongodb+srv://{consts.USERNAME}:{consts.PASSWORD}@cluster0.wyznt.mongodb.net/{consts.DB_NAME}?retryWrites=true&w=majority"
+    client = MongoClient(conn_str, serverSelectionTimeoutMS=5000)
+    db = client.test
+    return db
 
 
 def valid_email(email):
     regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
     return re.fullmatch(regex, email)
 
-def get_email(data):
-    json_data = json.loads(data) #TODO change to json.load when geting a json file
-    if not "email" in json_data.keys():
-        return False #TODO! ex? no email 
-    email = json_data["email"]
+def get_data():
+
+    json_file = None #TODO get the json file from the front end and return python dict
+
+    json_data = json.loads(json_file) #TODO change to json.load when geting a json file
+    return json_data
+
+def save_data(db, data):
+    if not "email" in data.keys():
+        raise Exception("Email field not exists") #? Do you have better exception for this one?
+    
+    email = data["email"]
     if not valid_email(email):
-        return False #TODO! ex? invalid email
-    else:
-        return True # retorn email adress
+        raise Exception("Unvalid email address") #? And do you have better exception for this one?
+
+    if db.users.find_one({"email": email}) != None:
+        raise Exception("Email address is already exists in the database") #? And for this one?
+
+    timestamp = datetime.now().strftime(r"%d/%m/%Y, %H:%M:%S") #TODO - time zone ? israel or utc, but not machine time!
+    #? do we need to use _id field for each user and if so how to generate it
+    #? but if we dont mongo add something like - '_id': ObjectId('612b6386ca405a7a4b7a1591')
+
+    db.users.insert_one({"email": email, "timestamp": timestamp})
 
 
-#TODO
-#obj = {"email": email, "time": datetime.now().strftime("%d/%m/%Y, %H:%M:%S")}
-#obj.save in MongoDB
+def main():
+    db = db_connection()
+    
+    #data = get_data()
+    #save_data(db, data)
 
-print(datetime.now().strftime(r"%d/%m/%Y, %H:%M:%S"))
 
+if __name__ == '__main__':
+    main()
--- tests.py
+++ tests.py
@@ -1,30 +1,60 @@
 import random
-from email import valid_email, get_email
+import json
+from land_page_back import valid_email, save_data, db_connection
 
         
+def email_gen(count):
+    for i in range(count):
+        mail = ''
+        alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',\
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',\
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',\
+            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',\
+            '_', '.', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        for i in range(random.randint(0, 17)):
+            mail += random.choice(alphabet)
+        mail += "@"
+        mail += random.choice(["gmail", "walla", "yahoo", "outlook"])
+        mail += random.choice([".com", ".co.il", ".ru", ".uk"])
 
-for i in range(500):
-    mail = ''
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',\
-        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',\
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',\
-        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',\
-        '_', '.', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    for i in range(random.randint(0, 17)):
-        mail += random.choice(alphabet)
-    mail += "@"
-    mail += random.choice(["gmail", "walla", "yahoo", "outlook"])
-    mail += random.choice([".com", ".co.il", ".ru", ".uk"])
+        yield mail
 
-    print(mail, '\t', valid_email(mail))
 
-#############################
+def check_email(emails):
+    for email in emails:
+        print("{:<28} {}".format(email, bool(valid_email(email))))
 
-json_string = """
-{
-    "name": "yehuda",
-    "email": "xiuw@dlhd.cdkv"
-}
-"""
+def check_db():
+    db = db_connection()
 
-print(get_email(json_string))
+    json_data = json.loads("""
+    {
+        "name": "yehuda",
+        "email": "yehuda@hfe.org"
+    }
+    """)
+
+    save_data(db, json_data)
+
+
+def print_db():
+    db = db_connection()
+    data = db.users.find()
+    for i in data:
+        print(i)
+
+
+
+def main():
+
+    check_email(email_gen(500))
+    
+    check_db()
+
+    print_db()
+
+
+
+
+if __name__ == '__main__':
+    main()
